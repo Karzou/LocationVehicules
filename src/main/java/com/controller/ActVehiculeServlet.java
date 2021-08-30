703ff122d7fccf3f2a13c5c3f29d2c52b03c3ccf
R3V3NG3 <petitpiqueur@gmail.com>
2021-08-30 16:44:07 +0200

    Scindé liste actif/inactif vehicule

---
+++ src/main/java/com/controller/ActVehiculeServlet.java
@@ -0,0 +1,88 @@
+package com.controller;
+
+import com.connection.EMF;
+import com.entity.Vehicule;
+import com.exception.ServiceException;
+import com.service.VehiculeService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Wets Jeoffroy
+ */
+
+@WebServlet("/actVehicule")
+public class ActVehiculeServlet extends HttpServlet {
+
+    final static Logger logger = LogManager.getLogger(ActVehiculeServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (logger.isInfoEnabled()) {
+
+            logger.info("Appel de la méthode \"doGet\" de la servlet \"ActVehiculeServlet\"");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (logger.isInfoEnabled()) {
+
+            logger.info("Appel de la méthode \"doPost\" de la servlet \"ActVehiculeServlet\"");
+        }
+
+        EntityManager em = EMF.getEM();
+        EntityTransaction transaction = em.getTransaction();
+
+        String id = request.getParameter("idSup");
+        int idSup = Integer.parseInt(id);
+
+        VehiculeService vehiculeService = new VehiculeService(em);
+        Vehicule vehicule = null;
+
+        try {
+
+            vehicule = vehiculeService.trouver(idSup);
+        } catch (ServiceException e) {
+
+            e.printStackTrace();
+        }
+
+        try {
+
+            transaction.begin();
+
+            vehicule.setActifVehicule(true);
+            vehiculeService.update(vehicule);
+
+            transaction.commit();
+        } catch ( Exception e ) {
+
+            throw new ServletException( e );
+        } finally {
+
+            if (transaction.isActive()) {
+
+                transaction.rollback();
+            }
+
+            if(logger.isInfoEnabled()) {
+
+                logger.info("Fermeture de l'EntityManager");
+            }
+
+            em.close();
+        }
+
+        response.sendRedirect("gestionVehicule");
+    }
+}
